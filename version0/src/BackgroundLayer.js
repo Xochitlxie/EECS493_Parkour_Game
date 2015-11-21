8880fa9674973a57ad9eb0d30faee185fe0811b0
yyibin <yyibin@umich.edu>
2015-11-21 13:45:19 -0500

    version0_1.7
    young 1:45

--- version0/src/BackgroundLayer.js
+++ version0/src/BackgroundLayer.js
@@ -24,6 +24,9 @@ var BackgroundLayer = cc.Layer.extend({
         this.map01.setPosition(cc.p(this.mapWidth, 0));
         this.addChild(this.map01);
 
+
+        this.scheduleUpdate();
+        
         // var winsize = cc.director.getWinSize();
  
         // //create the background image and position it at the center of screen
@@ -31,5 +34,29 @@ var BackgroundLayer = cc.Layer.extend({
         // var spriteBG = cc.Sprite.create(res.PlayBG_png);
         // spriteBG.setPosition(centerPos);
         // this.addChild(spriteBG);
+    },
+
+    checkAndReload:function (eyeX) {
+
+        var newMapIndex = parseInt(eyeX / this.mapWidth);
+        if (this.mapIndex == newMapIndex) {
+            return false;
+        }
+        if (0 == newMapIndex % 2) {
+            // change mapSecond
+            this.map01.setPositionX(this.mapWidth * (newMapIndex + 1));
+        } else {
+            // change mapFirst
+            this.map00.setPositionX(this.mapWidth * (newMapIndex + 1));
+        }
+        this.mapIndex = newMapIndex;
+        return true;
+    },
+
+    update:function (dt) {
+        var animationLayer = this.getParent().getChildByTag(TagOfLayer.Animation);
+        var eyeX = animationLayer.getEyeX();
+        this.checkAndReload(eyeX);
     }
+
 });
